0426a01d3bb2351bc24a9772fe3b9e12b8a07676
Rajat Garg <rajat@socialappshq.com>
2015-10-24 12:54:47 +0530

    Update shipment.rb

--- lib/fedex/request/shipment.rb
+++ lib/fedex/request/shipment.rb
@@ -108,14 +108,10 @@ module Fedex
 
       # Callback used after a failed shipment response.
       def failure_response(api_response, response)
-        puts "#############################################################################"
-        puts "#{api_response.inspect}"
-        puts "#############################################################################"
+      	puts "#############################################################################"
         puts "#{response.inspect}"
         puts "#############################################################################"
-        puts "#{response[:envelope][:body][:process_shipment_reply].inspect}"
-        puts "#############################################################################"
-
+        
         error_message = if response[:process_shipment_reply]
           [response[:process_shipment_reply][:notifications]].flatten.first[:message]
         else
