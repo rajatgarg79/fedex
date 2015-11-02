0cd582a1fccb56310a76bd618e9a412a74cc337d
Rajat Garg <rajat@socialappshq.com>
2015-11-02 16:24:46 +0530

    change soap client

--- lib/fedex/request/delete.rb
+++ lib/fedex/request/delete.rb
@@ -52,8 +52,9 @@ module Fedex
         puts "#######################################"
         puts "#{builder.doc.root.to_xml}"
         puts "#######################################"
-        return '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:v13="http://fedex.com/ws/ship/v13">
-<soapenv:Header/><soapenv:Body>#{builder.doc.root.to_xml}</soapenv:Body></soapenv:Envelope>'
+        return "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:v13=\"http://fedex.com/ws/ship/v13\">
+        <soapenv:Header/><soapenv:Body>#{builder.doc.root.to_xml}</soapenv:Body></soapenv:Envelope>"
+
       end
 
       def service
